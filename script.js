c2c5333858767d274cd5ba75fd875a41be6c994a
Michael Chong <mikec0920@gmail.com>
2021-09-01 20:12:22 -0700

    Added change color of square to blue on mouse hover

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const div = document.createElement('div');
     div.classList.add("grid-item");
-    //div.textContent = i * 16 + j + 1;
     container.appendChild(div);
   }
 }
 
-const gridItems = document.querySelectorAll("#grid-item");
+const gridItems = document.querySelectorAll(".grid-item");
 gridItems.forEach(gridItem => {
-  gridItem.addEventListener('mouseenter', () => {
-    console.log(gridItem);
+  gridItem.addEventListener('mouseover', () => {
     gridItem.style.backgroundColor = "blue";
   });
 });
