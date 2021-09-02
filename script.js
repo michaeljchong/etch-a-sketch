2eb18e61e76bc2151f609bcc3c81b9f4c11cb267
Michael Chong <mikec0920@gmail.com>
2021-09-01 21:06:27 -0700

    Added user input prompt to change grid size

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const container = document.getElementById("container");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
+function initializeGrid(gridSize) {
+  for (let i = 0; i < gridSize**2; i++) {
     const div = document.createElement('div');
     div.classList.add("grid-item");
     container.appendChild(div);
   }
 }
 
-const gridItems = document.querySelectorAll(".grid-item");
-gridItems.forEach(gridItem => {
-  gridItem.addEventListener('mouseover', () => {
-    gridItem.style.backgroundColor = "blue";
+function changeGridColor() {
+  const gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach(gridItem => {
+    gridItem.addEventListener('mouseover', () => {
+      gridItem.style.backgroundColor = "blue";
+    });
   });
-});
+}
+
+function clearScreen() {
+  const gridSize = prompt("How many squares per side?", 16);
+  container.innerHTML = "";
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  initializeGrid(gridSize);
+  changeGridColor();
+}
+
+initializeGrid(16);
+changeGridColor();
+const clearScreenBtn = document.getElementById("clearScreenBtn");
+clearScreenBtn.addEventListener('click', clearScreen);
